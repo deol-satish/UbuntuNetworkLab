641860adf338a7e01b88cb069c9d64da7e7b51b9
Deol Satish <164171952+deol-satish@users.noreply.github.com>
2025-03-27 10:18:23 +1100

    Added New scripts to copy keys shutdown and restart

--- Network Setup Script/copy_keys.sh
+++ Network Setup Script/copy_keys.sh
@@ -4,12 +4,12 @@
 set -x
 
 # Source the settings file
-if [ ! -f ../utils/settings.sh ]; then
+if [ ! -f ./utils/settings.sh ]; then
     echo "Error: settings.sh file not found"
     exit 1
 fi
 
-source ../utils/settings.sh
+source ./utils/settings.sh
 
 ssh-copy-id root@"$router_ipaddr"
 ssh-copy-id root@"$client1_ipaddr"
