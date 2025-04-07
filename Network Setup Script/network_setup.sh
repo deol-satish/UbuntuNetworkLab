50991fe5860e055b5ec8aa0fc0c4ee31993affa5
Deol Satish <164171952+deol-satish@users.noreply.github.com>
2025-04-07 14:11:20 +1000

    added tcp_test 211-client-router-server-2-innet

--- Network Setup Script/network_setup.sh
+++ Network Setup Script/network_setup.sh
@@ -44,8 +44,8 @@ sudo ip link set ens37 up;
 
 client2_setup_commands="
 sudo ip link set ens37 down;
-sudo ip addr add 192.168.2.2/24 dev ens37;
-sudo ip route add default via 192.168.2.1;
+sudo ip addr add 192.168.1.3/24 dev ens37;
+sudo ip route add default via 192.168.1.1;
 sudo ip link set ens37 up;
 "
 
@@ -54,7 +54,6 @@ server_setup_commands="
 sudo ip link set ens37 down;
 sudo ip addr add 192.168.3.2/24 dev ens37;
 sudo ip route add 192.168.1.0/24 via 192.168.3.1;
-sudo ip route add 192.168.2.0/24 via 192.168.3.1;
 sudo ip link set ens37 up;
 "
 
--- Network Setup Script/shutdown.sh
+++ Network Setup Script/shutdown.sh
@@ -4,12 +4,12 @@
 set -x
 
 # Source the settings file
-if [ ! -f ./utils/settings.sh ]; then
+if [ ! -f ../utils/settings.sh ]; then
     echo "Error: settings.sh file not found"
     exit 1
 fi
 
-source ./utils/settings.sh
+source ../utils/settings.sh
 
 
 ssh root@"$router_ipaddr" "$sudo shutdown -h now" &
